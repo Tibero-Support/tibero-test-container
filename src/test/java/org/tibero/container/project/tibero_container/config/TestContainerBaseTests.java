dd47f75bc9ee05e729a5408fd5b54ba2c9dbf62a
birdie <flaehdan@gmail.com>
2024-09-04 14:16:13 +0900

    하나의 테스트 컨테이너를 테스트가 공유하도록 설정

--- src/test/java/org/tibero/container/project/tibero_container/config/TestContainerBaseTests.java
+++ src/test/java/org/tibero/container/project/tibero_container/config/TestContainerBaseTests.java
@@ -2,22 +2,16 @@ package org.tibero.container.project.tibero_container.config;
 
 import java.io.File;
 import java.time.Duration;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.DockerComposeContainer;
 import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Import({TestConfig.class})
 @EnableConfigurationProperties
-@Testcontainers
 public abstract class TestContainerBaseTests {
 
 	@Container
@@ -27,9 +21,12 @@ public abstract class TestContainerBaseTests {
 				Wait.forLogMessage(".*For details.*", 1)
 					.withStartupTimeout(Duration.ofMinutes(5)));
 
+	static {
+		environment.start();
+	}
+
 	@DynamicPropertySource
 	static void properties(DynamicPropertyRegistry registry) {
-		environment.start();
 		registry.add("spring.db.driver-class-name", () -> "com.tmax.tibero.jdbc.TbDriver");
 		registry.add("spring.db.jdbc-url", () ->
 			String.format("jdbc:tibero:thin:@%s:%d:tibero",
@@ -39,5 +36,4 @@ public abstract class TestContainerBaseTests {
 		registry.add("spring.db.username", () -> "tibero");
 		registry.add("spring.db.password", () -> "tibero");
 	}
-
 }
